b2a4036fa3a6d9ac1e8989027495ce59be8c8251
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2021-02-25 12:59:35 +0100

    vista crear proyecto, .env , css crear proyecto

--- resources/views/principal/crearProyecto.blade.php
+++ resources/views/principal/crearProyecto.blade.php
@@ -1,4 +1,28 @@
 @extends("layouts.principal.layoutPrincipal")
+
+@section("titulo")
+    <title>PlanTool - Principal</title>
+@endsection
+@section("css")
+    <link href="css/crearProyecto.css" rel="stylesheet" />
+@endsection
 @section("contenido")
-    <h1>Crear Proyecto</h1>
+    <div class="container-fluid" id="contenedorPadre">
+        <div class="col-10 col-xl-7 mx-auto" id="contenedorFormulario">
+            <form action="#" method="post" class="col-11 col-lg-9 col-xl-7 mx-auto">
+            @csrf
+            <h1>Crear proyecto</h1>
+            <div>
+                <label for="titulo">Título: </label>
+                <input type="text" id="titulo" name="titulo">
+            </div>
+            <div>
+                <label for="desc">Descripción: </label>
+                <textarea id="desc" name="descripcion"></textarea>
+            </div>
+            <button type="submit">Crear</button>
+
+            </form>
+        </div>
+    </div>
 @endsection
