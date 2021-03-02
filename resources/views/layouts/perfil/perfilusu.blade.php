b64e0a6b316b8fe743545381186a28bdcd4fed43
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2021-03-02 13:27:42 +0100

    Reestablecido

--- resources/views/layouts/perfil/perfilusu.blade.php
+++
@@ -1,6 +0,0 @@
-<span><b>{{ \Illuminate\Support\Facades\Session::get($usuario)->nombre }} {{ \Illuminate\Support\Facades\Session::get($usuario)->apellidos }}</b></span>
-<br />
-<span>{{ \Illuminate\Support\Facades\Session::get($usuario)->usuario }}</span>
-<br />
-<span>{{ \Illuminate\Support\Facades\Session::get($usuario)->email }}</span>
-<br />
