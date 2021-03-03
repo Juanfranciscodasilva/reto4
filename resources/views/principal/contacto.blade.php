fc889d96983970c1ad037ad810b11ea97a28861e
Tania Garcia <taniagarcia@MacBook-Air-de-Tania.local>
2021-03-03 09:51:40 +0100

    puesta en comun

--- resources/views/principal/contacto.blade.php
+++ resources/views/principal/contacto.blade.php
@@ -9,16 +9,16 @@
 @section("contenido")
     <div class="container-fluid" id="contenedorPadre">
         <div class="col-12 col-xl-7 mx-auto" id="contenedorFormulario">
-            <form action="#" method="post" class="col-11 col-lg-9 col-xl-10 mx-auto">
+            <form action="#" method="post" class="col-11 col-lg-9 col-xl-11 mx-auto">
                 @csrf
                 <h1 class="m-0 pt-2 pb-2">Contacto</h1>
-                <div class="mt-4 text-md-center col-12 col-md-9 mx-auto">
+                <div class="mt-4 text-md-center col-12 col-md-11 mx-auto">
                     <label for="email">Email: </label>
                     <input type="text" id="email" name="email" class="text-md-center" value="{{ \Illuminate\Support\Facades\Session::get('usuario')->email }}" readonly>
                 </div>
-                <div class="text-md-center col-12 col-md-9 mx-auto">
+                <div class="text-md-center col-12 col-md-11 mx-auto">
                     <label for="desc">Mensaje: </label>
-                    <textarea id="desc" name="mensaje" class="text-md-center" required></textarea>
+                    <textarea id="men" name="mensaje" class="text-md-center" required></textarea>
                 </div>
                 <div class="col-12 col-md-9 mt-3 mt-xl-1 mx-auto">
                     <button type="submit" class="boton">Contactar</button>
